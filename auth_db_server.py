bee9ac820022d0ce52b0e0f600ed22d330d4ab9d
Søren Asger <au593128@uni.au.dk>
2022-03-24 19:50:52 +0100

    Some refactoring

--- auth_db_server.py
+++ auth_db_server.py
@@ -40,6 +40,7 @@ class MongoDB(DatabaseInterface):
     def insert_node(self, node: 'Node'):
         def create_mongodb_node():
             pass
+
         self.nodes.insert_one(create_mongodb_node())
 
     def remove_node(self, node: 'Node'):
@@ -47,6 +48,33 @@ class MongoDB(DatabaseInterface):
 
     def update_node(self, node: 'Node'):
         pass
+
+    def get_23node_by_id(self, node_id):
+        node = self.nodes.find_one({'_id': node_id})
+        # reads values and children and removes none values
+        values = node["values"].values()
+        children_ids = node["children"].values()
+        nod = Two3Node(node_id, values[0])
+        nod.right = values[1]
+        nod.left_child_id = children_ids[0]
+        nod.right_child_id = children_ids[2]
+        nod.mid_child_id = children_ids[1]
+        # should children be sorted?
+        return nod
+
+    def create_23_node(self, key, children=None) -> 'Two3Node':
+        if children is None:
+            children = [None, None]
+        node = {'children': {'left': children[0],
+                             'mid': None,
+                             'right': children[1]},
+                'values': {'left': key,
+                           'right': None}}
+        node_id = self.nodes.insert_one(node).inserted_id
+        return Two3Node(node_id, key)
+
+
+dbi = MongoDB()
 
 
 class Node:
@@ -64,6 +92,23 @@ class Node:
 
     def is_2_node(self):
         return len(self.values) == 1
+
+
+class Two3Node:
+
+    def __init__(self, node_id, left):
+        self.left = left
+        self.node_id = node_id
+        self.right = None
+        self.left_child_id = None
+        self.right_child_id = None
+        self.mid_child_id = None
+
+    def is_2_node(self):
+        return self.right is None
+
+    def __eq__(self, other: 'Two3Node'):
+        return self.node_id == other.node_id
 
 
 def find_nearest_node_and_parent(value):
@@ -162,34 +207,55 @@ def insert_empty_tree(value):
     nodes.insert_one(root)
 
 
-def insert_3_node_3_parent(value, insert_location, parent):
-    values = [insert_location["values"]["left"], insert_location["values"]["right"], value]
+def split_node(node, value, left_children=None, right_children=None):
+    values = [node.left, node.right, value]
     values.sort()
-    min = values[0]
-    mid = values[1]
-    max = values[2]
+    min_node = dbi.create_23_node(values[0], left_children)
+    max_node = dbi.create_23_node(values[2], right_children)
+    return min_node, max_node, values[1]
 
-    # Create 2 new nodes for min and max
-    min_node = {'children': {'left': None, 'mid': None, 'right': None}, 'values': {'left': min, 'right': None}}
-    max_node = {'children': {'left': None, 'mid': None, 'right': None}, 'values': {'left': max, 'right': None}}
-    min_node_id = nodes.insert_one(min_node).inserted_id
-    max_node_id = nodes.insert_one(max_node).inserted_id
 
-    parent_values = [parent["values"]["left"], parent["values"]["right"], mid]
-    parent_values.sort()
-    parent_min = parent_values[0]
-    parent_mid = parent_values[1]
-    parent_max = parent_values[2]
+def insert_3_node_3_parent(value, insert_location: 'Two3Node', parent: 'Two3Node'):
+    min_node, max_node, mid = split_node(insert_location, value)
 
+    # Now we need to reconstruct pointers from parent min and parent max to the children
+    left_child = dbi.get_23node_by_id(parent.left_child_id)
+    mid_child = dbi.get_23node_by_id(parent.mid_child_id)
+    right_child = dbi.get_23node_by_id(parent.right_child_id)
+
+    all_children = [left_child, mid_child, right_child, min_node, max_node]
+    # all_children contains the previous children (left, mid, right) and the new nodes (min and max)
+    # We now remove the node that was split and replaced with min and max
+    all_children = [child for child in all_children if child != insert_location]
+    all_children.sort(key=lambda x: x.left)
+
+    parent_min, parent_max, mid = split_node(parent, mid, left_children=all_children[:2],
+                                             right_children=all_children[2:])
+    parent.left_child_id = parent_min
+    parent.right_child_id = parent_max
+    parent.mid_child_id = None
+    # TODO: Update parent in database and delete insert_location from database
+    # TODO: Could maybe be more efficient to not delete insert_location and reuse it instead?
+    """
     # Create 2 new nodes for top min and top max
     # if inserted in left subtree
-    top_min_node = {'children': {'left': parent["children"]["left"], 'mid': None, 'right': parent["children"]["mid"]},
-                    'values': {'left': parent_min, 'right': None}}
-    top_max_node = {'children': {'left': min_node_id, 'mid': None, 'right': max_node_id},
-                    'values': {'left': parent_max, 'right': None}}
-    top_min_node_id = nodes.insert_one(top_min_node).inserted_id
-    top_max_node_id = nodes.insert_one(top_max_node).inserted_id
-    # if inserted in right subtree
+    inserted_in_left_subtree = None
+    if inserted_in_left_subtree:
+        left_subtree = {
+            'children': {'left': parent["children"]["left"], 'mid': None, 'right': parent["children"]["mid"]},
+            'values': {'left': parent_min, 'right': None}}
+        right_subtree = {'children': {'left': min_node_id, 'mid': None, 'right': max_node_id},
+                         'values': {'left': parent_max, 'right': None}}
+        top_min_node_id = nodes.insert_one(left_subtree).inserted_id
+        top_max_node_id = nodes.insert_one(right_subtree).inserted_id
+    else:  # no clue if this works
+        left_subtree = {'children': {'left': min_node_id, 'mid': None, 'right': max_node_id},
+                        'values': {'left': parent_min, 'right': None}}
+        right_subtree = {
+            'children': {'left': parent["children"]["mid"], 'mid': None, 'right': parent["children"]["right"]},
+            'values': {'left': parent_max, 'right': None}}
+        top_min_node_id = nodes.insert_one(left_subtree).inserted_id
+        top_max_node_id = nodes.insert_one(right_subtree).inserted_id
 
     # Put middle value in parent and update parent to point to the 2 new children
     parent["children"]["left"] = top_min_node_id
@@ -203,6 +269,7 @@ def insert_3_node_3_parent(value, insert_location, parent):
 
     # Delete old node
     nodes.delete_one({"_id": insert_location["_id"]})
+    """
 
 
 def insert_3_node_2_parent(value, insert_location, parent):
