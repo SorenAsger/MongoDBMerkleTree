7c7559888e17b39397b9c96387d017c69e0faa5f
jesperjensen <Jesperjon@outlook.dk>
2022-04-22 16:57:21 +0200

    cache me outside how 'bout dat?

--- cache.py
+++ cache.py
@@ -7,25 +7,23 @@ class Cache:
 
     def __init__(self, dbi):
         self.dbi = dbi
-        self.deleted = []
-        self.added = []
-        self.updated = []
+        self._deleted = []
+        self._added = {}
+        self._updated = {}
 
     def add(self, node):
         if node.node_id is None:
             node.node_id = str(ObjectId())
-        if node.node_id in self.deleted:
-            self.deleted.remove(node.node_id)
-        self.added[node.node_id] = node
+        if node.node_id in self._deleted:
+            self._deleted.remove(node.node_id)
+        self._added[node.node_id] = node
         return node
 
     def add_using_key_and_children(self, key, children=None) -> 'Two3Node':
         node_to_insert = Two3Node(None, key)
 
         if children is None:
-            node_to_insert.left_child_id = None
-            node_to_insert.mid_child_id = None
-            node_to_insert.right_child_id = None
+            children = [None, None, None]
 
         if len(children) == 3:
             node_to_insert.left_child_id = children[0]
@@ -38,11 +36,11 @@ class Cache:
         return self.add(node_to_insert)
 
     def get(self, node_id):
-        if node_id not in self.deleted:
-            if node_id in self.updated:
-                return self.updated[node_id]
-            if node_id in self.added:
-                return self.added[node_id]
+        if node_id not in self._deleted:
+            if node_id in self._updated:
+                return self._updated[node_id]
+            if node_id in self._added:
+                return self._added[node_id]
             else:
                 return self.dbi.get_23_node_by_id(node_id)
 
@@ -52,16 +50,22 @@ class Cache:
         # change the node that is to be added
         # to save an update.
         node_id = node.node_id
-        if node_id in self.added:
-            self.added[node_id] = node
+        if node_id in self._added:
+            self._added[node_id] = node
         else:
-            self.updated[node_id] = node
+            self._updated[node_id] = node
 
     def delete(self, node_id):
-        self.deleted.append(node_id)
+        self._deleted.append(node_id)
+
+    def reset(self):
+        self._deleted = []
+        self._added = {}
+        self._updated = {}
 
     def write_cache_to_db(self):
-        self.dbi.create_many_23_nodes_from_node(list(self.added.values()))
-        self.dbi.update_many_23_nodes(list(self.updated.values()))
-        self.dbi.delete_many_23_nodes(self.deleted)
+        self.dbi.delete_many_23_nodes(self._deleted)
+        self.dbi.create_many_23_nodes_from_node(list(self._added.values()))
+        self.dbi.update_many_23_nodes(list(self._updated.values()))
+        self.reset()
 
