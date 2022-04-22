df1c6143d02ca2b1d57cf6bcacf1adf2d4cafc01
Rasmus <sexygaltab@gmail.com>
2022-04-22 12:16:32 +0200

    added method to add from key and children

--- cache.py
+++ cache.py
@@ -1,3 +1,8 @@
+from bson import ObjectId
+
+from node import Two3Node
+
+
 class Cache:
 
     def __init__(self, dbi):
@@ -7,14 +12,35 @@ class Cache:
         self.updated = {}
 
     def add(self, node):
+        if node.node_id is None:
+            node.node_id = str(ObjectId())
         if node.node_id in self.deleted:
             self.deleted.remove(node.node_id)
         self.added[node.node_id] = node
+        return node
+
+    def add_using_key_and_children(self, key, children=None) -> 'Two3Node':
+        node_to_insert = Two3Node(None, key)
+
+        if children is None:
+            node_to_insert.left_child_id = None
+            node_to_insert.mid_child_id = None
+            node_to_insert.right_child_id = None
+
+        if len(children) == 3:
+            node_to_insert.left_child_id = children[0]
+            node_to_insert.mid_child_id = children[1]
+            node_to_insert.right_child_id = children[2]
+        else:
+            node_to_insert.left_child_id = children[0]
+            node_to_insert.right_child_id = children[1]
+
+        return self.add(node_to_insert)
 
     def get(self, node_id):
         if node_id not in self.deleted:
             if node_id in self.updated:
-                return self.added[node_id]
+                return self.updated[node_id]
             if node_id in self.added:
                 return self.added[node_id]
             else:
