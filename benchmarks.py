099e6478248f0418e6529edae0f9c5bf3867fa19
Rasmus <sexygaltab@gmail.com>
2022-04-27 12:40:30 +0200

    cleaned db interface

--- benchmarks.py
+++ benchmarks.py
@@ -5,30 +5,34 @@ import random
 import timeit
 import matplotlib.pyplot as plt
 
+from verifier import Verifier
 from cache import Cache
 from db_adapters import MongoDB
 from auth_db_server import AuthDBServer
 
 dbi = MongoDB()
-cache=Cache(dbi, write_to_db=False)
+cache = Cache(dbi, write_to_db=False)
 server = AuthDBServer(dbi, cache)
+verifier = Verifier(server)
+
 
 def insert_many(start, end):
     for i in range(start, end):
         server.insert(random.randint(0, 2 ** 128))
 
+
 def insert_sorted(start, end):
     for i in range(start, end):
         server.insert(i)
 
+
 def moving_average(x, w):
     return np.convolve(x, np.ones(w), 'valid') / w
 
-server.destroy_db()
-#cProfile.run('insert_sorted(0, 1000)')
 
-def plot_avg(n, interval_length, function):
-    server.destroy_db()
+# cProfile.run('insert_sorted(0, 1000)')
+
+def plot_avg_time(n, interval_length, function, titel, x_label, y_label, ma_weight):
     y_values = []
     x_values = []
 
@@ -36,35 +40,22 @@ def plot_avg(n, interval_length, function):
 
         start = timeit.default_timer()
         for i in range(j, j + interval_length):
-            if i == 13641:
-                print("hej")
-            else:
-                function(i)
+            function(i)
         end = timeit.default_timer()
 
         avg_y = (end - start) / interval_length
         y_values.append(avg_y)
         x_values.append(j)
 
-    for v in y_values:
-        if v > 5.0720000000015195 * 10 ** (-5) * 20:
-            print("hallelujah")
-            index = y_values.index(v)
-            print(v)
-            print("x: ", x_values[index])
-    plt.title("Avg. insertion time")
-    plt.xlabel("n")
-    plt.ylabel("t")
-    print(len(y_values))
-    print(y_values)
-    ma_w = 1
-    y_values = moving_average(y_values, ma_w).tolist()
-    print("After MA")
-    print(len(y_values))
-    print(y_values)
-    x_values = x_values[ma_w - 1:]
+    plt.title(titel)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+
+    y_values = moving_average(y_values, ma_weight).tolist()
+    x_values = x_values[ma_weight - 1:]
     plt.plot(x_values, y_values)
     plt.show()
+
 
 def plot_avg_insertion_time(n):
     server.destroy_db()
@@ -72,7 +63,7 @@ def plot_avg_insertion_time(n):
     x_values = []
 
     for i in range(1, n):
-        start_val = 2 ** (i-1)
+        start_val = 2 ** (i - 1)
         end_val = 2 ** i
 
         start = timeit.default_timer()
@@ -88,6 +79,7 @@ def plot_avg_insertion_time(n):
     plt.plot(x_values, exec_times)
     plt.show()
 
+
 def plot_avg_deletion_time(n):
     server.destroy_db()
     exec_times = []
@@ -97,7 +89,7 @@ def plot_avg_deletion_time(n):
     insert_sorted(0, 2 ** n)
 
     for i in range(1, n):
-        start_val = 2 ** (i-1)
+        start_val = 2 ** (i - 1)
         end_val = 2 ** i
 
         start = timeit.default_timer()
@@ -113,6 +105,7 @@ def plot_avg_deletion_time(n):
     plt.ylabel("t")
     plt.plot(x_values, exec_times)
     plt.show()
+
 
 def plot_membership_witness_size(n):
     server.destroy_db()
@@ -130,6 +123,7 @@ def plot_membership_witness_size(n):
     plt.ylabel("l")
     plt.plot(x_values, witness_lenghts)
     plt.show()
+
 
 def plot_non_membership_witness_size(n):
     server.destroy_db()
@@ -149,8 +143,10 @@ def plot_non_membership_witness_size(n):
     plt.plot(x_values, witness_lenghts)
     plt.show()
 
-#plot_avg_insertion_time(15)
-#plot_avg_deletion_time(15)
-#plot_membership_witness_size(1000)
-#plot_non_membership_witness_size(1000)
-plot_avg(22000, 10, server.insert)
+
+server.destroy_db()
+n = 10000
+plot_avg_time(n, 10, server.insert, "Avg. insertion time", "s", "n", 1)
+plot_avg_time(n, 10, verifier.verify_membership, "Avg. verification time", "s", "n", 1)
+
+plot_avg_time(n, 10, server.delete, "Avg. deletion time", "s", "n", 1)
