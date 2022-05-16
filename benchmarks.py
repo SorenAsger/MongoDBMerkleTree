619d052ea2e59c1010c53c92e06a213d4dfb571d
RasmusCC <sexygaltab@gmail.com>
2022-05-16 15:16:24 +0200

    fixed witness benchmark.

--- benchmarks.py
+++ benchmarks.py
@@ -70,7 +70,9 @@ def get_avg_witness_time(n, interval_length, function, random_writes=False, inpu
 
         start = timeit.default_timer()
         for i in range(0, amount_of_witnesses):
-            function(random.randint(0, j))
+            # all values in [0, j + interval_length
+            # should be in the DB at this point in time.
+            function(random.randint(0, j + interval_length))
         end = timeit.default_timer()
 
         avg_y = (end - start) / amount_of_witnesses
@@ -133,8 +135,8 @@ def plot(x_values, y_values, titel, x_label, y_label, ma_weight, filename, scien
     y_values = moving_average(y_values, ma_weight).tolist()
     x_values = x_values[ma_weight - 1:]
     plt.plot(x_values, y_values)
+    plt.savefig("plots/" + filename)
     plt.show()
-    plt.savefig(filename)
 
 
 server.destroy_db()
@@ -145,28 +147,26 @@ time_label = "seconds"
 length_label = "bytes"
 
 x_vals, y_vals = get_avg_time(n, interval, server.insert)
-plot(x_vals, y_vals, "Avg. insertion time", "total values in DB", time_label, ma, filename="hej")
+plot(x_vals, y_vals, "Avg. insertion time", "total values in DB", time_label, ma, filename="insertion")
 
-'''
 x_vals, y_vals = get_avg_time(n, interval, server.delete)
-plot(x_vals, y_vals, "Avg. deletion time", "total values in DB", time_label, ma)
+plot(x_vals, y_vals, "Avg. deletion time", "total values in DB", time_label, ma, filename="deletion")
 
 x_vals, y_vals = get_avg_witness_time(n, interval, server.get_membership_proof)
-plot(x_vals, y_vals, "Avg. membership witness generation time", "total values in DB", time_label, ma)
+plot(x_vals, y_vals, "Avg. membership witness generation time", "total values in DB", time_label, ma, filename="wit_gen_time")
 
 x_vals, y_vals = get_avg_witness_time(n, interval, server.get_non_membership_proof, input_factor=-1)
-plot(x_vals, y_vals, "Avg. non-membership witness generation time", "total values in DB", time_label, ma)
+plot(x_vals, y_vals, "Avg. non-membership witness generation time", "total values in DB", time_label, ma, filename="non_mem_wit_gen_time")
 
 x_vals, y_vals = get_avg_witness_length(n, interval, server.get_membership_proof)
-plot(x_vals, y_vals, "Avg. membership witness length", "total values in DB", length_label, ma_weight=1, scientific_y=False)
+plot(x_vals, y_vals, "Avg. membership witness length", "total values in DB", length_label, ma_weight=1, scientific_y=False, filename="wit_length")
 
 x_vals, y_vals = get_avg_witness_length(n, interval, server.get_non_membership_proof, input_factor=-1)
-plot(x_vals, y_vals, "Avg. non-membership witness length", "total values in DB", length_label, ma_weight=1, scientific_y=False)
+plot(x_vals, y_vals, "Avg. non-membership witness length", "total values in DB", length_label, ma_weight=1, scientific_y=False, filename="non_mem_wit_length")
 
 x_vals, y_vals = get_avg_witness_time(n, interval, verifier.verify_membership)
-plot(x_vals, y_vals, "Avg. membership witness verification time", "total values in DB", time_label, ma)
+plot(x_vals, y_vals, "Avg. membership witness verification time", "total values in DB", time_label, ma, filename="wit_ver_time")
 
 x_vals, y_vals = get_avg_witness_time(n, interval, verifier.verify_non_membership)
-plot(x_vals, y_vals, "Avg. non-membership witness verification time", "total values in DB", time_label, ma)
+plot(x_vals, y_vals, "Avg. non-membership witness verification time", "total values in DB", time_label, ma, filename="non_mem_wit_ver_time")
 
-'''
